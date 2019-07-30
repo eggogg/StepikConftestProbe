6fa1be811cb87bc2fe723e7e0b96477cfacebf4c
Igor Yu.Saltykov <eggogg@gmail.com>
2019-07-30 23:59:57 +0400

    add pytest files

---
+++ test_items.py
@@ -0,0 +1,5 @@
+﻿link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_should_be_on_the_page(browser):
+    browser.get(link)
+    browser.find_element_by_class_name("btn-add-to-basket")
